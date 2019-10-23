90730cb5fa1c34f50318f7ea84465824f8029030
Alireza Torkamani <na.s.taaherian@hotmail.com>
2019-10-23 10:35:41 -0400

    added translation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'parsiNail-UI';
+  param = {value: 'ParsiNail'};
+  constructor(private translate: TranslateService) {
+    translate.setDefaultLang('fa');
+    translate.use('fa');
+  }
+  onChangeLanguage() {
+    this.translate.currentLang === 'fa' ? this.translate.use('en') : this.translate.use('fa');
+  }
 }
